267aba4acdcc1545158820416c6c7809aec3fa4b
shizhangchun <shichangchun@jd.com>
2022-03-15 20:14:09 +0800

    add

--- test.js
+++ test.js
@@ -1,18 +1,34 @@
-function transform(num,int){
-  if(Number.isInteger(num)&&Number.isInteger(int)&&int<=16&&int>1){
-    let temp = '0123456789ABCDEF'
-    let yu = 0
-    let arr = []
-    while(num){
-      yu = num%int
-      num = Math.floor(num/int)
-      arr.push(temp[yu])
-    }
-    return arr.reverse().join('')
-  }else{
-    console.log('参数有误')
-  }
-  
-}
-let r1 = transform(100,16)
-console.log(r1)
+setTimeout(function(){
+  console.log('111')
+  new Promise((reslove,reject)=>{
+    reslove()
+  }).then(function(){
+    console.log('2222')
+  })
+},0)
+
+
+setTimeout(function(){
+  console.log('333')
+  new Promise((reslove,reject)=>{
+    reslove()
+  }).then(function(){
+    console.log('4444')
+  })
+},0)
+
+new Promise((reslove,reject)=>{
+  reslove()
+}).then(function(){
+  console.log('555')
+})
+
+console.log('666')
+
+// 执行结果
+// 666
+// 555
+// 111
+// 333
+// 2222
+// 4444
