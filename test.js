081a81dcb87fa2ae5825dbb4ff6cd506956335a6
shizhangchun <shichangchun@jd.com>
2022-03-16 17:36:00 +0800

    add

--- test.js
+++ test.js
@@ -1,21 +1,28 @@
-// 输入：l1 = [2,4,3], l2 = [5,6,4]
-// 输出：[7,0,8]
-// 解释：342 + 465 = 807.
-
-let l1 = [2,4,3], l2 = [5,6,6]
-
-function add21(l1,l2){
-  function sum(l){
-    let totle = l.reduce(function(totle,current,index,arr){
-       totle += current*Math.pow(10,index)
-       return totle
-    },0)
-    console.log(totle)
-    return totle
+let s = 'abcafghjfrghc'
+/**
+ * 通过双指针循环
+ * @param {} s 
+ * @returns 
+ */
+function test2(s){
+  let map = {}
+  let arr = []
+  let len = s.length
+  let moveKey = 0
+  let max = 0
+  for(let i=0;i<len;i++){
+    if(i!=0){
+      arr.shift()
+    }
+    while(moveKey<len&&!arr.includes(s[moveKey])){
+      arr.push(s[moveKey])
+      moveKey++
+    }
+    max = Math.max(max,moveKey-i)
+    map[i]=s.slice(i,moveKey)
   }
-  let s = sum(l1)+sum(l2)
-  console.log(s)
-  return String(s).split('').reverse().map(item => Number(item))
-
+  console.log(map)
+  return max 
 }
-console.log(add21([6,4,3],[5,6,6]))
+
+console.log(test2(s))
