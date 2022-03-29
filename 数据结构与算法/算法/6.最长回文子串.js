c81840af132997242d8d5884225df53784815a8a
shizhangchun <shichangchun@jd.com>
2022-03-29 17:08:59 +0800

    add

--- 数据结构与算法/算法/6.最长回文子串.js
+++ 数据结构与算法/算法/6.最长回文子串.js
@@ -5,7 +5,7 @@
  * @param {*} str 
  */
 
-function longestPalindromicSubstring(str){
+function test1(str){
   let len = str.length
   if(len<2){
     return str
@@ -17,12 +17,11 @@ function longestPalindromicSubstring(str){
     let currMax = str1.length>=str2.length?str1:str2
     maxStr = maxStr.length>=currMax.length?maxStr:currMax
   }
-  console.log('maxStr ', maxStr)
   return maxStr
   function findStr(left,right){
     let maxstr = ''
     while(left>-1&&right<len&&str[left]==str[right]){
-      maxstr = str.slice(left,right+1)
+      maxstr = str.slice(left,right+1) // 此处值得优化
       left--
       right++
     }
@@ -30,6 +29,42 @@ function longestPalindromicSubstring(str){
   }
 }
 
-let str = 'bb'
+var test2 = function(str) {
+  let len = str.length
+  if(len<2){
+    return str
+  }
+  left=0
+  maxLen=1
+  for(let i=0;i<len;i++){
+    let str1 = findStr(i-1,i+1)
+    let str2 = findStr(i,i+1)
+    if(str1[1]>str2[1]){
+      if(str1[1]>maxLen){
+        left = str1[0]
+        maxLen = str1[1]
+      }
+    } else {
+      if(str2[1]>maxLen){
+        left = str2[0]
+        maxLen = str2[1]
+      }
+    }
+  }
+  return str.slice(left,left+maxLen)
+  function findStr(left,right){
+    let cLen = 0
+    let cLeft = 0
+    while(left>-1&&right<len&&str[left]==str[right]){
+      cLen = right - left + 1
+      cLeft = left
+      left--
+      right++
+    }
+    return [cLeft,cLen]
+  }
+};
 
-longestPalindromicSubstring(str)
+let str = 'cbbd'
+
+console.log(test2(str))
