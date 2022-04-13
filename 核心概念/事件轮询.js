13cbb2cb20761464bad1d087c46302bb1bf55203
shizhangchun <shichangchun@jd.com>
2022-04-13 18:31:51 +0800

    add

--- 核心概念/事件轮询.js
+++ 核心概念/事件轮询.js
@@ -1,30 +1,92 @@
-setTimeout(function(){
-  console.log('111')
-  new Promise((reslove,reject)=>{
-    console.log('aaa')
-    reslove()
-  }).then(function(){
-    console.log('2222')
-  })
-},0)
+// setTimeout(function(){
+//   console.log('111')
+//   new Promise((reslove,reject)=>{
+//     console.log('aaa')
+//     reslove()
+//   }).then(function(){
+//     console.log('2222')
+//   })
+// },0)
 
+
+// setTimeout(function(){
+//   console.log('333-50')
+//   new Promise((reslove,reject)=>{
+//     console.log('bbb-50')
+//     reslove()
+//   }).then(function(){
+//     console.log('4444-50')
+//   })
+// },5)
+// setTimeout(function(){
+//   console.log('333-60')
+//   new Promise((reslove,reject)=>{
+//     console.log('bbb-60')
+//     reslove()
+//   }).then(function(){
+//     console.log('4444-60')
+//   })
+// },6)
 
 setTimeout(function(){
   console.log('333')
   new Promise((reslove,reject)=>{
     console.log('bbb')
+    setTimeout(function(){
+      console.log('8888')
+    },4)
     reslove()
   }).then(function(){
     console.log('4444')
+  }).then(function(){
+    console.log('7777')
+  })
+
+  new Promise((reslove,reject)=>{
+    console.log('bbb-2')
+    setTimeout(function(){
+      console.log('8888-2')
+    },4)
+    reslove()
+  }).then(function(){
+    console.log('4444-2')
+  }).then(function(){
+    console.log('7777-2')
   })
 },0)
+setTimeout(function(){
+  console.log('ss333')
+  new Promise((reslove,reject)=>{
+    console.log('ssbbb')
+    setTimeout(function(){
+      console.log('ss8888')
+    },4)
+    reslove()
+  }).then(function(){
+    console.log('ss4444')
+  }).then(function(){
+    console.log('ss7777')
+  })
 
-new Promise((reslove,reject)=>{
-  console.log('ccc')
-  reslove()
-}).then(function(){
-  console.log('555')
-})
+  new Promise((reslove,reject)=>{
+    console.log('ssbbb-2')
+    setTimeout(function(){
+      console.log('ss8888-2')
+    },4)
+    reslove()
+  }).then(function(){
+    console.log('ss4444-2')
+  }).then(function(){
+    console.log('ss7777-2')
+  })
+},5)
+
+// new Promise((reslove,reject)=>{
+//   console.log('ccc')
+//   reslove()
+// }).then(function(){
+//   console.log('555')
+// })
 
 console.log('666')
 
@@ -34,7 +96,13 @@ console.log('666')
 // 555
 // 111
 // aaa
+// 2222
 // 333
 // bbb
-// 2222
 // 4444
+// 333-50
+// bbb-50
+// 4444-50
+// 333-60
+// bbb-60
+// 4444-60
