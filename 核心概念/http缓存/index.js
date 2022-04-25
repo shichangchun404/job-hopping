207bf189094bd775072ec1738e8d17c16c1139a2
shizhangchun <shichangchun@jd.com>
2022-04-25 17:26:48 +0800

    add

--- 核心概念/http缓存/index.js
+++ 核心概念/http缓存/index.js
@@ -5,9 +5,14 @@ var etag = require('etag')
 const app = express()
 
 app.use('*',(req,res,next)=>{
-  // console.log('请求进来了 url= ',req.url)
+  console.log('请求进来了 url= ',req.url)
   next()
 })
+
+// app.use(express.static(path.resolve(__dirname,'./static'), {
+//   etag: false,
+//   lastModified: false,
+// }))
 
 app.get('/',(req,res,next)=>{
   let data = fs.readFileSync(path.resolve(__dirname,'./index.html'))
@@ -19,18 +24,14 @@ app.get('/',(req,res,next)=>{
 app.get('/1.jpg',(req,res)=>{
   console.log('/1.jpg ')
   let data = fs.readFileSync(path.resolve(__dirname,'./static/1.jpg'))
-  fs.stat(path.resolve(__dirname,'./static/1.jpg'),(err,stat)=>{
-    // console.log(stat)
-  })
   res.setHeader('Content-Type','image/jpeg')
   res.setHeader('Cache-Control','max-age=30')
   res.send(data)
 })
 app.get('/2.jpg',(req,res)=>{
-  console.log('/2.jpg ', req.headers)
+  console.log('/2.jpg ')
   let data = fs.readFileSync(path.resolve(__dirname,'./static/2.jpg'))
   const ETag = etag(data)
-  console.log('etag == ', ETag)
   res.setHeader('ETag',ETag)
   res.setHeader('Content-Type','image/jpeg')
   res.setHeader('Cache-Control','no-cache')
@@ -53,7 +54,6 @@ app.get('/3.jpg',(req,res)=>{
   res.setHeader('Content-Type','image/jpeg')
   res.setHeader('Cache-Control','no-cache')
   res.setHeader('Last-Modified',mtime)
-  // console.log(req.headers)
   if(req.headers['if-modified-since']&&mtime==req.headers['if-modified-since'].toString()){
     console.log('文件修改时间一致 返回304')
     res.statusCode = 304
@@ -63,6 +63,15 @@ app.get('/3.jpg',(req,res)=>{
   let data = fs.readFileSync(path.resolve(__dirname,'./static/3.jpg'))
   res.send(data)
 })
+
+app.get('/4.jpg',(req,res)=>{
+  console.log('/4.jpg ')
+  let data = fs.readFileSync(path.resolve(__dirname,'./static/4.jpg'))
+  res.setHeader('Content-Type','image/jpeg')
+  res.send(data)
+})
+
+
 app.listen(8848,()=>{
   console.log('app start at http://localhost:8848')
 })
